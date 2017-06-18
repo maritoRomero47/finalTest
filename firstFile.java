630db19dc839f32f35a91b3fded97f84c708b81f
Mario Romero <mcromero47@gmail.com>
2017-06-18 17:16:16 -0300

    commit number One #1

---
+++ firstFile.java
@@ -0,0 +1,42 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class EmployeeSerializator {
+
+	public static void main(String[] args) 
+	{
+		Employee emp = new Employee();
+		emp.name = "mario";
+		emp.lastName = "cesar";
+		
+		FileOutputStream fout = null;
+		ObjectOutputStream oout = null;
+		try
+		{
+			fout = new FileOutputStream("employee.txt");
+			oout = new ObjectOutputStream(fout);
+			
+			oout.writeObject(emp);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			try {
+				oout.flush();
+				oout.close();
+				fout.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
