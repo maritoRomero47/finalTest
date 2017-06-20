c56a67b580105a6a40cc1ffa42ab99dfca996bfb
Mario Romero <mcromero47@gmail.com>
2017-06-20 20:31:01 -0300

    commit of changes in secondFile from US002

--- secondFile.java
+++ secondFile.java
@@ -1,6 +1,7 @@
 /**
  * This is just a file to test
- * a merge between branches and
- * what happens with the files
+ * a merge between branches, commit
+ * and try to ask for a pull request
+ * to see what happens with the files
  * added.
  */
